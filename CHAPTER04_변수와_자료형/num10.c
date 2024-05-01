9cf1b00e6d473705092c42ffd06ce6aed3cfa7dd
among5094 <106166621+among5094@users.noreply.github.com>
2024-05-01 17:22:03 +0900

    Add files via upload

---
+++ CHAPTER04_변수와_자료형/num10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//10번
+int main() {
+    // 첫 번째 경보음
+    printf("\a");
+
+    // 메시지 출력
+    printf("경보! 시스템에 문제가 발생하였습니다.\n");
+
+    // 두 번째 경보음
+    printf("\a");
+
+    return 0;
+}
---
+++ CHAPTER04_변수와_자료형/num11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//11번
+int main() {
+    // 문자와 특수 문자 출력
+    printf("\"ASCII code\", 'A', 'B', 'C'\n");
+    printf("\\t \\a \\n\n");
+
+    return 0;
+}
